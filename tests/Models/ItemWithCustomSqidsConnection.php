acd07473206564dcecc5cb5e10c7208a5dd5476d
Niels Postma <niels@postisit.nl>
2025-11-28 12:10:12 +0100

    Forgot to commit the added Factory

--- tests/Models/ItemWithCustomSqidsConnection.php
+++ tests/Models/ItemWithCustomSqidsConnection.php
@@ -4,12 +4,15 @@ namespace ErikSulymosi\EloquentSqids\Tests\Models;
 
 use ErikSulymosi\EloquentSqids\Eloquent\Traits\HasSqid;
 use ErikSulymosi\EloquentSqids\Eloquent\Traits\SqidRouting;
+use ErikSulymosi\EloquentSqids\Tests\Factories\ItemWithCustomSqidsConnectionFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ItemWithCustomSqidsConnection extends Model
 {
     use HasSqid;
     use SqidRouting;
+    use HasFactory;
 
     protected $guarded = [];
 
